7ba65f77e3afe44618d5a6b07b4eb95f4f1fbc5e
Mapelujo <olamideiyanda18@gmail.com>
2024-07-24 15:04:07 +0100

    fetching data from the data.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
+import { useState, useEffect } from "react";
+// import dataJson from "./assets/data.json";
+
 export default function App() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(function () {
+    async function getComments() {
+      const res = await fetch('./data.json');
+      // console.log(res);
+      const data = await res.json();
+      // console.log(data.comments);
+      setComments(data.comments)
+    }
+
+    getComments();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-light-grey min-h-[100vh] border">
+      <main className="absolute left-[50%] -translate-x-1/2 w-[40%] my-10">
+        {/* <UserComment /> */}
+
+        {comments.map(comment => <UserComment comment={comment} key={comment.id}/>)}
+      </main>
+    </div>
+  );
+}
+
+function UserComment({comment}) {
+  return (
+    <div className="bg-white p-5 flex gap-5 rounded-lg mb-5">
+      <section className="bg-light-grey basis-[6.5%] py-1 flex flex-col gap-1 items-center rounded-lg h-[5.4rem]">
+        <div className="text-greyish-blue font-semibold cursor-pointer">+</div>
+        <h2 className="text-moderate-blue font-bold">{comment.score}</h2>
+        <div className="text-greyish-blue font-semibold cursor-pointer">-</div>
+      </section>
+      <section className="basis-[92%]  border-soft-red">
+        <article className="flex items-center justify-between">
+          <div className="flex items-center justify-between basis-[42%]">
+            <img
+              className="w-[11%]"
+              src={comment.user.image.png}
+              alt="amyrobson-image"
+            ></img>
+            <h6 className="text-dark-blue font-bold">
+              {comment.user.username}
+              <span className="text-sm text-greyish-blue font-medium ml-2">
+                {comment.createdAt}
+              </span>
+            </h6>
+          </div>
+          <div className="flex items-center gap-2 border-soft-red cursor-pointer">
+            <img src='./images/icon-reply.svg' alt="reply-icon"></img>
+            <h4 className="text-moderate-blue font-bold">Reply</h4>
+          </div>
+        </article>
+        <article className="border-moderate-blue mt-3">
+          <p className="text-greyish-blue text-sm">
+            {comment.content}
+          </p>
+        </article>
+      </section>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        "moderate-blue": "hsl(238, 40%, 52%)",
+        "soft-red": "hsl(358, 79%, 66%)",
+        "light-greyish-blue": "hsl(239, 57%, 85%)",
+        "pale-red": "hsl(357, 100%, 86%)",
+        "dark-blue": "hsl(212, 24%, 26%)",
+        "greyish-blue": "hsl(211, 10%, 45%)",
+        "light-grey": "hsl(223, 19%, 93%)",
+        "very-light-grey": "hsl(228, 33%, 97%)",
+        "white": "hsl(0, 0%, 100%)",
+      }
     },
   },
   plugins: [],
