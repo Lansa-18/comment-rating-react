c7368e9ac40f35bb2f773ec36c95d4afe346ab07
Mapelujo <olamideiyanda18@gmail.com>
2024-07-26 15:42:11 +0100

    fixed the comments and replies component issue

--- src/components/InputFromUser.js
+++ src/components/InputFromUser.js
@@ -2,20 +2,20 @@ export default function InputFromUser({
   inpValue,
   onSetValue,
   curUser,
+  curUserImg,
   onUserInput,
 }) {
   return (
     <div className="flex border justify-between bg-white p-4 rounded-lg">
       <img
         className="w-[5%] h-[5%]"
-        src="./images/avatars/image-juliusomo.png"
+        src={curUserImg}
         alt="current-user-icon"
       ></img>
       <form className="border border-greyish-blue-opaque w-[75%] rounded-md">
         <input
           value={inpValue}
           onChange={(e) => onSetValue(e)}
-          type="textarea"
           className="p-3 placeholder:text-greyish-blue w-full border border-none outline-none rounded-md"
           placeholder="Add a comment..."
         ></input>
