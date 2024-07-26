102092e2a1280a0051e7b4c73b97c12f43ac3340
Mapelujo <olamideiyanda18@gmail.com>
2024-07-26 11:31:28 +0100

    separating components into different files

---
+++ src/components/InputFromUser.js
@@ -0,0 +1,31 @@
+export default function InputFromUser({
+  inpValue,
+  onSetValue,
+  curUser,
+  onUserInput,
+}) {
+  return (
+    <div className="flex border justify-between bg-white p-4 rounded-lg">
+      <img
+        className="w-[5%] h-[5%]"
+        src="./images/avatars/image-juliusomo.png"
+        alt="current-user-icon"
+      ></img>
+      <form className="border border-greyish-blue-opaque w-[75%] rounded-md">
+        <input
+          value={inpValue}
+          onChange={(e) => onSetValue(e)}
+          type="textarea"
+          className="p-3 placeholder:text-greyish-blue w-full border border-none outline-none rounded-md"
+          placeholder="Add a comment..."
+        ></input>
+      </form>
+      <button
+        className="bg-moderate-blue text-white font-bold rounded-lg px-5 py-3"
+        onClick={onUserInput}
+      >
+        Send
+      </button>
+    </div>
+  );
+}
---
+++ src/components/UserComment.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+export default function UserComment({ comment }) {
+  const [voteCount, setVoteCount] = useState(comment.score);
+
+  function handleVoteCount(e) {
+    if (e.target.textContent === "+") {
+      setVoteCount((prev) => prev + 1);
+    }
+    if (e.target.textContent === "-") {
+      setVoteCount((prev) => prev - 1);
+    }
+  }
+
+  return (
+    <div className="bg-white p-5 flex gap-5 rounded-lg mb-5">
+      <section
+        onClick={handleVoteCount}
+        className="bg-light-grey basis-[6.5%] py-1 flex flex-col gap-1 items-center rounded-lg h-[5.4rem]"
+      >
+        <div className="text-greyish-blue font-semibold cursor-pointer">+</div>
+        <h2 className="text-moderate-blue font-bold">{voteCount}</h2>
+        <div className="text-greyish-blue font-semibold cursor-pointer">-</div>
+      </section>
+      <section className="basis-[92%]  border-soft-red">
+        <article className="flex items-center justify-between">
+          <div className="flex items-center justify-between basis-[42%]">
+            <span className="flex border-soft-red items-center w-[61%]">
+              <img
+                className="w-[17%] mr-4"
+                src={comment.user.image.png}
+                alt="amyrobson-image"
+              ></img>
+              <h6 className="text-dark-blue font-bold">
+                {comment.user.username}
+              </h6>
+            </span>
+            <span className="text-sm text-greyish-blue font-medium">
+              {comment.createdAt}
+            </span>
+          </div>
+          <div className="flex items-center gap-2 border-soft-red cursor-pointer">
+            <img src="./images/icon-reply.svg" alt="reply-icon"></img>
+            <h4 className="text-moderate-blue font-bold">Reply</h4>
+          </div>
+        </article>
+        <article className="border-moderate-blue mt-3">
+          <p className="text-greyish-blue text-sm">
+            {comment.replyingTo && (
+              <span className="font-bold text-moderate-blue">
+                {`@${comment?.replyingTo} `}
+              </span>
+            )}
+            {comment?.content}
+          </p>
+        </article>
+      </section>
+    </div>
+  );
+}
